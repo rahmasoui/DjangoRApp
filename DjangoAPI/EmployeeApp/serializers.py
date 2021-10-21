788f03c464a2163a7e04f8a105a2e76a6a8999c9
RahmaSoui <rahma.souissi.8@gmail.com>
2021-10-21 09:14:46 +0100

    first commit

---
+++ DjangoAPI/EmployeeApp/serializers.py
@@ -0,0 +1,20 @@
+"""
+Serializer to convert code python into json and xml easly
+"""
+from rest_framework import serializers
+from EmployeeApp.models import Departments, Employees
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Departments
+        fields = ('DepartmentId',
+                  'DepartmentName')
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employees
+        fields = ('EmployeeId',
+                  'EmployeeName',
+                  'Department',
+                  'DateOfJoining',
+                  'PhotoFileName')
